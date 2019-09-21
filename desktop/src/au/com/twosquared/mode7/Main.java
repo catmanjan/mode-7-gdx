20e1396981b65cf34f0a89c7b990d044664f21b7
Humberto Dias <hldtux@gmail.com>
2019-09-22 00:58:14 +0200

    Adding gradle + gdx 1.9.10

---
+++ desktop/src/au/com/twosquared/mode7/Main.java
@@ -0,0 +1,15 @@
+package au.com.twosquared.mode7;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "mode-7-gdx";
+		cfg.width = 800;
+		cfg.height = 600;
+		
+		new LwjglApplication(new Game(), cfg);
+	}
+}
